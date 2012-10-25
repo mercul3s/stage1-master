b5a4923e669d5a756dfa0f37f6f025cfe76fe144
Christian <moonlimb@gmail.com>
2012-10-25 11:02:38 -0700

    added some url_for magic to our app

--- tipsy.py
+++ tipsy.py
@@ -1,7 +1,7 @@
 """
 tipsy.py -- A flask-based todo list
 """
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import model
 
 app = Flask(__name__)
